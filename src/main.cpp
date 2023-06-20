6ef8aa6c01ce3646f05451edc7aab4749febad1b
fungikami <kafung09@gmail.com>
2023-06-20 00:58:36 -0400

    minor fix

--- src/main.cpp
+++ src/main.cpp
@@ -105,9 +105,9 @@ int main(int argc, const char **argv) {
         float start_time = Utils::read_time_in_seconds(); {
         try {
             value = (algorithm == 1) ? negamax(pv[i], npv, color, use_tt)
-                : (algorithm == 2) ? negamax(pv[i], npv, INT_MIN, INT_MAX, color, use_tt)
+                : (algorithm == 2) ? negamax(pv[i], npv, -INT_MAX, INT_MAX, color, use_tt)
                 : (algorithm == 3) ? scout(pv[i], npv, color, use_tt)
-                : negascout(pv[i], npv, INT_MIN, INT_MAX, color, use_tt);
+                : negascout(pv[i], npv, -INT_MAX, INT_MAX, color, use_tt);
         } catch (const bad_alloc &e) {
             cout << "size TT[0]: size=" << TTable[0].size() << ", #buckets=" << TTable[0].bucket_count() << endl;
             cout << "size TT[1]: size=" << TTable[1].size() << ", #buckets=" << TTable[1].bucket_count() << endl;
