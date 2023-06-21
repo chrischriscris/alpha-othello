ec7ce3bf79537e17694fa97c3358731ce8715317
chrischriscris <18-10892@usb.ve>
2023-06-20 23:12:34 -0400

    modified runall (too much heat)

--- src/runall.sh
+++ src/runall.sh
@@ -3,20 +3,27 @@
 # Create ../results/tests/ directory if it doesn't exist
 mkdir -p ../results/tests/
 
-# Run the 4 algorithms (without TT) in parallel with a timeout of 1 hour
-timeout 3600 ./alpha-othello 1 0 > ../results/alpha-othello-1-0.txt &
-timeout 3600 ./alpha-othello 2 0 > ../results/alpha-othello-2-0.txt &
-timeout 3600 ./alpha-othello 3 0 > ../results/alpha-othello-3-0.txt &
-timeout 3600 ./alpha-othello 4 0 > ../results/alpha-othello-4-0.txt &
-
-# Wait for the 4 algorithms to finish
-sleep 3600
-
-# 10 more minutes for the cpu to cool down
+# Run the 4 algorithms (without TT) with a timeout of 1 hour
+timeout 3600 ./alpha-othello 1 0 > ../results/alpha-othello-1-0.txt
 sleep 600
 
-# Run the 4 algorithms (with TT) in parallel with a timeout of 1 hour
-timeout 3600 ./alpha-othello 1 1 > ../results/alpha-othello-1-1.txt &
-timeout 3600 ./alpha-othello 2 1 > ../results/alpha-othello-2-1.txt &
-timeout 3600 ./alpha-othello 3 1 > ../results/alpha-othello-3-1.txt &
-timeout 3600 ./alpha-othello 4 1 > ../results/alpha-othello-4-1.txt &
+timeout 3600 ./alpha-othello 2 0 > ../results/alpha-othello-2-0.txt
+sleep 600
+
+timeout 3600 ./alpha-othello 3 0 > ../results/alpha-othello-3-0.txt
+sleep 600
+
+timeout 3600 ./alpha-othello 4 0 > ../results/alpha-othello-4-0.txt
+sleep 600
+
+# Run the 4 algorithms (with TT) with a timeout of 1 hour
+timeout 3600 ./alpha-othello 1 1 > ../results/alpha-othello-1-1.txt
+sleep 600
+
+timeout 3600 ./alpha-othello 2 1 > ../results/alpha-othello-2-1.txt
+sleep 600
+
+timeout 3600 ./alpha-othello 3 1 > ../results/alpha-othello-3-1.txt
+sleep 600
+
+timeout 3600 ./alpha-othello 4 1 > ../results/alpha-othello-4-1.txt
